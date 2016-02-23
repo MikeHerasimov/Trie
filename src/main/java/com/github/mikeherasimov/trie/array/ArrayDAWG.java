3da7781d65ae1417bda772c6c99b8ea9b913fdb0
Mike Herasimov <21jhonnycash@gmail.com>
2016-02-23 17:13:06 +0200

    ArrayDAWG's Builder and LinkedDAWG`s Builder was removed

--- src/main/java/com/github/mikeherasimov/trie/array/ArrayDAWG.java
+++ src/main/java/com/github/mikeherasimov/trie/array/ArrayDAWG.java
@@ -1,6 +1,5 @@
 package com.github.mikeherasimov.trie.array;
 
-import com.github.mikeherasimov.trie.Alphabet;
 import com.github.mikeherasimov.trie.DAWG;
 
 /**
@@ -11,53 +10,6 @@ public final class ArrayDAWG implements DAWG {
 
     ArrayDAWG(ArrayTrie trie){
         this.trie = trie;
-    }
-
-    /**
-     * Builder is inner class, which can be used to create ArrayDAWG object from supplied words.
-     */
-    public static class Builder{
-        private ArrayTrie trie;
-
-        /**
-         * Returns Builder object, supported by specified alphabet
-         *
-         * @param alphabet  specified alphabet
-         */
-        public Builder(String alphabet){
-            trie = new ArrayTrie(alphabet);
-        }
-
-        /**
-         * Returns new Builder object, supported by specified Alphabet`s instance.
-         *
-         * @param alphabet  specified <code>Alphabet</code>`s instance
-         */
-        public Builder(Alphabet alphabet){
-            trie = new ArrayTrie(alphabet);
-        }
-
-        /**
-         * Adds specified words to builder and returns it
-         *
-         * @param word  specified word
-         * @return      <code>Builder</code> object
-         */
-        public Builder put(String word){
-            trie.add(word);
-            return this;
-        }
-
-        /**
-         * Generates new ArrayDAWG object from supplied words and returns it.
-         * Note that this method doesn't creates copy of ArrayTrie that it holds
-         * it just makes optimization of it to DAWG and returns respective ArrayDAWG object.
-         *
-         * @return  respective <code>ArrayDAWG</code> object
-         */
-        public ArrayDAWG build(){
-            return trie.toDAWG();
-        }
     }
 
     @Override
--- src/main/java/com/github/mikeherasimov/trie/linked/LinkedDAWG.java
+++ src/main/java/com/github/mikeherasimov/trie/linked/LinkedDAWG.java
@@ -1,6 +1,5 @@
 package com.github.mikeherasimov.trie.linked;
 
-import com.github.mikeherasimov.trie.Alphabet;
 import com.github.mikeherasimov.trie.DAWG;
 
 /**
@@ -11,60 +10,6 @@ public final class LinkedDAWG implements DAWG {
 
     LinkedDAWG(LinkedTrie trie){
         this.trie = trie;
-    }
-
-    /**
-     * Builder is inner class, which can be used to create LinkedDAWG object from supplied words.
-     */
-    public static class Builder{
-        private LinkedTrie trie;
-
-        /**
-         * Returns Builder object
-         */
-        public Builder(){
-            trie = new LinkedTrie();
-        }
-
-        /**
-         * Returns Builder object, supported by specified alphabet
-         *
-         * @param alphabet  specified alphabet
-         */
-        public Builder(String alphabet){
-            trie = new LinkedTrie(alphabet);
-        }
-
-        /**
-         * Returns new Builder object, supported by specified Alphabet`s instance.
-         *
-         * @param alphabet  specified <code>Alphabet</code>`s instance
-         */
-        public Builder(Alphabet alphabet){
-            trie = new LinkedTrie(alphabet);
-        }
-
-        /**
-         * Adds specified words to builder and returns it
-         *
-         * @param word  specified word
-         * @return      <code>Builder</code> object
-         */
-        public Builder put(String word){
-            trie.add(word);
-            return this;
-        }
-
-        /**
-         * Generates new LinkedDAWG object from supplied words and returns it.
-         * Note that this method doesn't creates copy of LinkedTrie that it holds
-         * it just makes optimization of it to DAWG and returns respective LinkedDAWG object.
-         *
-         * @return  respective <code>LinkedDAWG</code> object
-         */
-        public LinkedDAWG build(){
-            return trie.toDAWG();
-        }
     }
 
     @Override
